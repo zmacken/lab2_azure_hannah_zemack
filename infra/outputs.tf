87a7659017c904436e39ff9c7e40f11a451e8860
Hannah Zemack <hannah.zemack@gmail.com>
2026-09-02 13:08:34 +0200

    outputs

---
+++ infra/outputs.tf
@@ -0,0 +1,15 @@
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "container_app_name" {
+  value = azurerm_container_app.api.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
