a5a02d5e3a253061597a57018d882c2b6cbc9f91
Hannah Zemack <hannah.zemack@gmail.com>
2026-09-03 09:41:32 +0200

    dashboard

--- backend/src/backend/data_processing.py
+++ backend/src/backend/data_processing.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd
 from backend.constants import DATA_PATH
 
@@ -6,11 +7,13 @@ df = pd.read_csv(DATA_PATH / "solar.csv")
 
 #fill missing values in the "Path Width (km)" and "Central Duration" columns with "None"
 df["Path Width (km)"] = df["Path Width (km)"].fillna("None")
-df["Central Duration"] = df["Central Duration"].fillna("None")
+df["Central Duration"] = df["Central Duration"].fillna("00m00s")
 
 #convert the "Central Duration" column from a string format (e.g., "00m00s") to seconds
 def duration_to_seconds(val):
-    if val == "00m00s" or val == "None" or pd.isna(val):
+
+    print(val)
+    if val == "00m00s" or val == "-":
         return 0
     minutes, seconds = val.replace("m", " ").replace("s", "").split()
     return int(minutes) * 60 + int(seconds)
@@ -20,3 +23,4 @@ df["Central Duration (s)"] = df["Central Duration"].apply(duration_to_seconds)
 #extract the year from the "Calendar Date" column and and remake to a number
 df["Year"] = df["Calendar Date"].str.extract(r"(-?\d+)").astype(int) 
 
+#%%
--- frontend/src/frontend/dashboard.py
+++ frontend/src/frontend/dashboard.py
@@ -1,15 +1,31 @@
 import streamlit as st
 import httpx
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
 BASE_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
 def main():
     st.markdown("# Solar Data Dashboard")
-    st.write(BASE_URL)
 
     stats = httpx.get(f"{BASE_URL}/solar-data/csv", timeout=30).json()
     st.dataframe(stats)
+
+    df = pd.DataFrame(stats)
+
+    st.subheader("Antal solförmörkelser per typ")
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    df["Eclipse Type"].value_counts().plot(kind="bar", ax=ax, color="steelblue")
+    ax.set_title("Antal solförmörkelser per typ")
+    ax.set_xlabel("Typ")
+    ax.set_ylabel("Antal")
+    plt.tight_layout()
+
+    st.pyplot(fig)
+
+
 
 if __name__ == "__main__":
     main()
